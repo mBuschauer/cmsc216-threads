5c139a275ff89f1b4134275776ba16cce903aeeb
mBuschauer <marco@buschauer.us>
2024-11-21 16:38:43 -0500

    finished draft

--- main.c
+++ main.c
@@ -2,19 +2,20 @@
 #include "sum.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+
+void randomArray(int seed, int numElements, int **array);
 
 int main() {
-	/*
-		code here handles the shell input.
-		 - creates array of random integer values
-		 - takes command line arguments and passes to max and sum
-		 - outputs the result of max and sum
-		 - will handle displaying wall clock time, the user time and the system/kernel time
-	*/
-	int size, threads, seed, task;
+	int size, threads, seed, task, output;
+	int *array;
 	char print;
-	
+
+	struct timeval start_wall, end_wall;
+	struct rusage start_usage, end_usage;
+	double wall_time, user_time, sys_time;
+
 	printf("# of elements in list?: ");
 	scanf(" %d", &size);
 	printf("# of threads: ");
@@ -25,6 +26,51 @@ int main() {
 	scanf(" %d", &task);
 	printf("Print Results? (Y/N): ");
 	scanf(" %c", &print);
+
+	randomArray(seed, size, &array);
+
+	gettimeofday(&start_wall, NULL);
+	getrusage(RUSAGE_SELF, &start_usage);
+
+	switch (task) {
+	case 1:
+		output = max(array, size, threads);
+	case 2:
+		output = sum(array, size, threads);
+	default:
+		output = -1;
+	}
+
+	gettimeofday(&end_wall, NULL);
+	getrusage(RUSAGE_SELF, &end_usage);
+
+    wall_time = (end_wall.tv_sec - start_wall.tv_sec) + 
+                (end_wall.tv_usec - start_wall.tv_usec) / 1e6;
+    user_time = (end_usage.ru_utime.tv_sec - start_usage.ru_utime.tv_sec) + 
+                (end_usage.ru_utime.tv_usec - start_usage.ru_utime.tv_usec) / 1e6;
+    sys_time = (end_usage.ru_stime.tv_sec - start_usage.ru_stime.tv_sec) + 
+               (end_usage.ru_stime.tv_usec - start_usage.ru_stime.tv_usec) / 1e6;
+
+
+	if (print == 'Y' || print == 'y') {
+		printf("Output: %d\n", output);
+	}
+
+    printf("Wall Clock Time: %f seconds\n", wall_time);
+    printf("User Time: %f seconds\n", user_time);
+    printf("System Time: %f seconds\n", sys_time);
+
+	free(array);
+	return 0;
+}
+
+void randomArray(int seed, int numElements, int **array) {
+	int i;
+	*array = (int *)calloc(numElements, sizeof(int));
+	srand(seed);
+	for (i = 0; i < numElements; i++) {
+		(*array)[i] = rand();
+	}
 }
 
 /*
